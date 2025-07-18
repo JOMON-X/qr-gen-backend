0b4ebdd80342c974ad39ed998c32a399c0a3973e
JOMON-X <mjouhar1999@gmail.com>
2025-07-18 11:05:51 +0530

    11th commit i guess

--- api/app.js
+++ api/app.js
@@ -1,29 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const serverless = require('serverless-http')
 const cors = require('cors')
-const DB = require('./DB')
-const userRoute = require('./route/userRoute')
+const DB = require('../DB')
+const userRoute = require('../route/userRoute')
 
 
 const app = express()
 
 //middlewares
-app.use(cors({
-    origin:'*'
-}))
 app.use(bodyParser.json())
 app.use(express.json());
+app.use(cors({origin:'*'}))
 
-//test route
-app.get('/ping', (req, res) => {
-    console.log("Ping endpoint hit!");
-  res.send('pong');
-})
+
 
 //all routes initiate here
 app.use('/userApi',userRoute)
 
-console.log("APP RUNNING")
+
 
 module.exports = app
+module.exports.handler = serverless(app)
--- api/index.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../app');
-
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled rejection:', err);
-});
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught exception:', err);
-});
