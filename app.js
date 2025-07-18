081d573a29229042cfb319c3d33bd1349b7f21b2
JOMON-X <mjouhar1999@gmail.com>
2025-07-18 20:53:01 +0530

    23rd commit trying out for render

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-console.log("APP STARTING");
-
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
-
-console.log("CONNECTING TO DB");
-
-// const DB = require('./DB')
-// const userRoute = require('./route/userRoute')
-
+const DB = require('./DB')
+const userRoute = require('./route/userRoute')
 
 const app = express()
 
@@ -19,20 +12,9 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use(cors({origin:'*'}))
 
-app.get('/',(req,res)=>{
-    res.send("hello from vercel")
-}) 
-
-app.get('/api/ping', (req, res) => {
-    console.log("HIT");
-  res.send('pong');
-});
-
-console.log("APP SETUP COMPLETE");
-
 
 //all routes initiate here
-// app.use('/userApi',userRoute)
+app.use('/userApi',userRoute)
 
 
 
