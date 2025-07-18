534775499c6f3beb0572b32ea31bd451c767b741
JOMON-X <mjouhar1999@gmail.com>
2025-07-18 12:45:07 +0530

    18th commit changing db connection

--- DB.js
+++ DB.js
@@ -1,12 +1,16 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL)
-
-mongoose.connection.on("connected",()=>{
-    console.log('\x1b[32m%s\x1b[0m','DB connected');
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-
-mongoose.connection.on("error",(error)=>{`error is ${error}`})
+  .then(() => {
+    console.log('\x1b[32m%s\x1b[0m', '✅ DB connected');
+  })
+  .catch((error) => {
+    console.error('❌ DB connection error:', error);
+    process.exit(1); // Stops the function to prevent timeout
+  });
 
 module.exports = mongoose
