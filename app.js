27cae1006d8d340c06da04cf54499fcd91437ace
JOMON-X <mjouhar1999@gmail.com>
2025-07-24 10:44:05 +0530

    minor adjustments

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 //middlewares
 app.use(bodyParser.json())
 app.use(express.json());
-app.use(cors({origin:'*'}))
+app.use(cors({origin:'https://ultimateqr.onrender.com'}))
 
 
 //all routes initiate here
